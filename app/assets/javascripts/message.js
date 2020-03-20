b2798016ad04356d32d7144a17e44cb505685493
porunga-udon <torico_trans_am@yahoo.co.jp>
2020-03-20 19:45:37 +0900

    konami command

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,10 @@
 $(function(){
+
+  // 入力キー配列
+  var input_cmd = [];
+  // コナミコマンド配列 [↑, ↑, ↓, ↓, ←, →, ←, →, B, A]
+  var konami_cmd = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+  
       function buildHTML(message,current_user_name){
         if ( message.image ) {
           if (message.user_name == current_user_name) {
@@ -107,4 +113,24 @@ $(function(){
       $('.form__submit').prop('disabled', false);
     });
   })
+
+  $(".group-tabs__tab").click(function(){
+
+  })
+
+
+  $(window).keyup(function(event) {
+    input_cmd.push(event.keyCode);
+    if(input_cmd[input_cmd.length - 1] != konami_cmd[input_cmd.length - 1]){
+      input_cmd = [];
+    } else if (input_cmd.length == konami_cmd.length) {
+      // alert("コナミコマンド発動！");
+      $(".wrapper").addClass('konami');
+      javascript:(function () {var s = document.createElement('script');
+        s.setAttribute('src', 'http://fontbomb.ilex.ca/js/main.js');
+        document.body.appendChild(s);}());
+      input_cmd = [];
+    }
+  });
+    
 });
