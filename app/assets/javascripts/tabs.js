feb1efd26577eb3a00bb471794f29402f2b7954c
porunga-udon <torico_trans_am@yahoo.co.jp>
2020-03-27 20:37:16 +0900

    tab mouseover mouseleave

--- app/assets/javascripts/tabs.js
+++ app/assets/javascripts/tabs.js
@@ -4,4 +4,18 @@ $(function(){
     console.log("ok");
   })
 
+  $(".group-tabs__tab").mouseover(function(){
+    $(this).stop().animate({ 
+      color: "black",
+      backgroundColor: "white"
+    },200)
+  })
+
+  $(".group-tabs__tab").mouseleave(function(){
+    $(this).stop().animate({
+      color: "white",
+      backgroundColor: "#253141"
+    },1000);
+  })
+
 })
