6e3d6d6da1dad8cd0094c5f22743a899bd18c26a
porunga-udon <torico_trans_am@yahoo.co.jp>
2020-03-28 11:00:03 +0900

    group-tab change-color

--- app/assets/javascripts/tabs.js
+++ app/assets/javascripts/tabs.js
@@ -1,21 +1,21 @@
 $(function(){
 
-  $(".group-tabs__tab").click(function(){
-    console.log("ok");
-  })
-
   $(".group-tabs__tab").mouseover(function(){
     $(this).stop().animate({ 
-      color: "black",
       backgroundColor: "white"
+    },200)
+    $(this).find(".group-name").stop().animate({ 
+      color: "black"
     },200)
   })
 
   $(".group-tabs__tab").mouseleave(function(){
     $(this).stop().animate({
-      color: "white",
       backgroundColor: "#253141"
     },1000);
+    $(this).find(".group-name").stop().animate({ 
+      color: "whiete"
+    },1000)
   })
 
 })
